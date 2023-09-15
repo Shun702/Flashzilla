c32b732f053ca50e1d4476dd8dcea14b23270c16
Shun702 <138392252+Shun702@users.noreply.github.com>
2023-09-15 13:35:18 +0100

    initial commit
    initial code

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Shun Le Yi Mon on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
